7beb0ea0c9b7736273189797580c1eec50e6a9a1
Julian Hermanspahn <julian.hermanspahn@mni.thm.de>
2020-08-24 13:09:57 +0200

    Initial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+    production: false,
+    firebaseConfig: {
+        apiKey: 'AIzaSyBQBMrszmm9qRG2yUfh0142TwbKDJL06gA',
+        authDomain: 'pictures-app-68662.firebaseapp.com',
+        databaseURL: 'https://pictures-app-68662.firebaseio.com',
+        projectId: 'pictures-app-68662',
+        storageBucket: 'pictures-app-68662.appspot.com',
+        messagingSenderId: '910839890576',
+        appId: '1:910839890576:web:57572b24f8089d4f16ce15',
+        measurementId: 'G-2D6RD4R1GR'
+    }
 };
 
 /*
