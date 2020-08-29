b0ac284072c01c4a90e9301823b59f51019cf63e
Julian Hermanspahn <julian.hermanspahn@mni.thm.de>
2020-08-29 19:17:34 +0200

    landing

--- src/app/pages/landing/landing.page.ts
+++ src/app/pages/landing/landing.page.ts
@@ -1,29 +1,48 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {AuthService} from '../../services/auth/auth.service';
 import {Router} from '@angular/router';
+import {IonSlides} from '@ionic/angular';
 
 @Component({
     selector: 'app-landing',
     templateUrl: './landing.page.html',
-    styleUrls: ['./landing.page.scss'],
+    styleUrls: ['./landing.page.scss', './responsive.scss'],
 })
 export class LandingPage {
+    @ViewChild('slideWithNav', {static: false}) slideWithNav: IonSlides;
 
-    slideOpts = {
-        initialSlide: 1,
-        speed: 400
+    sliderOne = {
+        isBeginningSlide: true,
+        isEndSlide: false,
+        slidesItems: [
+            {
+                path: './assets/icon/quiz.svg',
+                text: 'Teste dein Wissen!'
+            },
+            {
+                path: './assets/icon/statistics.svg',
+                text: 'Messe deinen Fortschritt!'
+            },
+            {
+                path: './assets/icon/achievement.svg',
+                text: 'Erreiche Abzeichen!'
+            }
+        ]
+    };
+
+    slidesOptions: any = {
+        zoom: {
+            toggle: false
+        },
+        initialSlide: 0,
+        loop: true,
+        slidesPerView: 1,
+        autoplay: true,
+        grabCursor: true
     };
 
     constructor(private authService: AuthService,
                 private router: Router) {
-    }
-
-    redirectToLogin() {
-        this.router.navigate(['/login']);
-    }
-
-    redirectToRegister() {
-        this.router.navigate(['/registrierung']);
     }
 
     googleLogin() {
