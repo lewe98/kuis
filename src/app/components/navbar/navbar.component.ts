a5ff06997e188924975921d8ffaac89e7f29159d
Tim Hoffmann <tim.hoffmann@mni.thm.de>
2020-08-30 22:07:06 +0200

    Doku für navbar.component.ts

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,16 @@
 import {Component, Input} from '@angular/core';
-
 @Component({
     selector: 'app-navbar',
     templateUrl: './navbar.component.html',
     styleUrls: ['./navbar.component.scss'],
 })
+
+/**
+ * Manages the inputs that are necessary for the correct display of the Navbar.
+ *
+ * @param name: Manages the title of the page the navbar is displayed on. The title will be displayed centered in the Navbar.
+ * @param showLogout: Either displays an additional Logout-Button in the HTML or not. Uses the logout-component.
+ */
 export class NavbarComponent {
     @Input() name: string;
     @Input() showLogout: boolean;
