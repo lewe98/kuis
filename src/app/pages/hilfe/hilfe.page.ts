8b0218e139eff17842bf0b883e416ceb23680e46
Julian Hermanspahn <julian.hermanspahn@mni.thm.de>
2020-09-10 17:36:26 +0200

    hilfe

--- src/app/pages/hilfe/hilfe.page.ts
+++ src/app/pages/hilfe/hilfe.page.ts
@@ -1,14 +1,63 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {AuthService} from '../../services/auth/auth.service';
+import {IonInput, ViewDidEnter} from '@ionic/angular';
 
 @Component({
     selector: 'app-hilfe',
     templateUrl: './hilfe.page.html',
     styleUrls: ['./hilfe.page.scss'],
 })
-export class HilfePage {
+export class HilfePage implements ViewDidEnter {
 
-    constructor() {
+    @ViewChild(IonInput) search: IonInput;
+    showText = false;
+    filteredFragenArray = [];
+    fragenArray = [
+        {
+            f: 'Wie starte ich ein Quiz?',
+            a: 'Du musst...',
+            show: false
+        },
+        {
+            f: 'Wie erreiche ich ein Abzeichen?',
+            a: 'Du musst...',
+            show: false
+        },
+        {
+            f: 'Wie lösche ich mein Profil?',
+            a: 'Du musst...',
+            show: false
+        }];
+
+    constructor(public authService: AuthService) {
+        this.filteredFragenArray = this.fragenArray;
     }
 
+    async showAntwort(frage: string) {
+        await this.fragenArray.forEach(e => {
+            if (e.f === frage) {
+                e.show = !e.show;
+            } else {
+                e.show = false;
+            }
+        });
+    }
 
+    async doSearch() {
+        const input = await this.search.getInputElement();
+        const searchValue = input.value;
+        this.filteredFragenArray = this.fragenArray.filter(f => {
+            return f.f.toLowerCase().includes(searchValue.toLowerCase()) ||
+                f.a.toLowerCase().includes(searchValue.toLowerCase());
+        });
+    }
+
+    clear() {
+        this.search.value = '';
+        this.filteredFragenArray = this.fragenArray;
+    }
+
+    ionViewDidEnter() {
+        setTimeout(() => this.search.setFocus(), 10);
+    }
 }
