50e1ecc476091be9db4f8e77683c147e73559a30
Mario <mario.mollame@mni.thm.de>
2020-09-20 22:55:03 +0200

    reduce timer for question

--- src/app/components/frage/frage.component.ts
+++ src/app/components/frage/frage.component.ts
@@ -131,7 +131,7 @@ export class FrageComponent {
             }
             setTimeout(() => {
                 this.showNextQuestion();
-            }, 2500);
+            }, 1000);
         } else {
             // TODO: - Style (rot, Wackeln)
             alert('falsch :(');
@@ -140,7 +140,7 @@ export class FrageComponent {
             }
             setTimeout(() => {
                 this.showNextQuestion();
-            }, 2500);
+            }, 1000);
         }
     }
 
